9cb618eeb9427b1f617a2e805af8ab20711ffd49
APRENDIZ <APRENDIZ@BOGAPRCSFFSD332.FormacionSena.RED>
2022-10-21 14:38:23 -0500

    Agregando estilos

--- PythonWeb/PythonWeb/views.py
+++ PythonWeb/PythonWeb/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 
 def index(request):
     return render (request,'index.html', {
+        'nombre':"Diego Murillo",
+        'edad':18
+    })
 
+def contact(request):
+    return render (request,'contact.html', {
     })
